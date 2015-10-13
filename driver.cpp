a6ea239b2baeb2929aa9a13ef5d9c9d9f244d9dc
Sarah Villegas <sarahbvillegas@student.hartnell.edu>
2015-10-12 18:54:44 -0700

    added function menu() and spelunk()

--- driver.cpp
+++ driver.cpp
@@ -18,3 +18,38 @@ int main()
 {
     return 0;   
 }
+
+
+char menu(){ 
+
+    char choice;
+
+    cout << "Would you like to go to Devil's Courthouse Adventure Weekend enter D" <<endl; 
+    cout << "Would you like to go to Bahama Scuba enter B" <<endl;
+    cout << "Would you like to go to Colorado Sky Dive enter C" <<endl;  
+    cout << "Would you like to go to Spelunk Barron Cliff enter S" <<endl;  
+    cin >> choice; 
+    
+    return choice; 
+}
+
+double spelunk(int num){
+
+    double baseCharge = 700.0;
+    double equitmentCharge = 40.0 * 8.0;
+    string needEquipment;
+    double totalAmount = 0.0;
+    cout << "Do you need to rent equitment for your adventure? ";
+    cin >> needEquipment; 
+    
+    if ((needEquipment == "yes") || (needEquipment == "Yes")){
+    
+        totalAmount += num * equitmentCharge; 
+    
+    }
+    totalAmount += baseCharge * num;
+
+    //totalAmount = dicount(num, totalAmount);
+    
+    return totalAmount; 
+}
